1231cc8500f1fef7db9ace7b76b41a426d2b7b3c
scook23 <35343383+scook23@users.noreply.github.com>
2018-02-08 11:54:14 -0600

    beginning of javascript

---
+++ js/code-assignment 03.js
@@ -0,0 +1,11 @@
+let projectId = 1
+let projectTitle = "Story Map"
+let thumbnail = true
+let projectTitles = ["Story Map", "Book Assignment 1: Web Story map of RJ, Brasil"]
+
+console.log('ID: ' + projectId)
+console.log('URL: ' + projectUrl)
+console.log('Title: ' + projectTitle)
+console.log('Description: ' + projectDesc)
+console.log('Has thumbnail? ' + projectThumb)
+console.log('Map extent: ' + projectMapExtent)
