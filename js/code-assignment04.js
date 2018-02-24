8ec67325d2aa751e3402f82bb258fa28859d451f
scook23 <35343383+scook23@users.noreply.github.com>
2018-02-24 11:55:34 -0600

    code assignment 4

--- js/code-assignment04.js
+++ js/code-assignment04.js
@@ -1,31 +1,18 @@
-// assign values to a set of variables representing a project's metadata
-let projectId = 1
-let projectUrl = 'http://arcg.is/2n5E9Jf'
-let projectTitle = 'Book Assignment 1: Web Story map of RJ, Brasil'
-let projectDesc = 'Web Story map of RJ, Brasil'
-let projectThumb = true
-
-// create an array and assign six variables to positions 0 through 5
 let projectArray = [
-  projectId,
-  projectUrl,
-  projectTitle,
-  projectDesc,
-  projectThumb,
+  1,
+  'http://arcg.is/2n5E9Jf',
+  'Book Assignment 1: Web Story map of RJ, Brasil',
+  'Web Story map of RJ, Brasil',
+  true,
+  ['Brasil','tour'],
 ]
 
-// create an object and assign six variables to property names (i.e., keys)
 let projectObject = {
-  id: projectId,
-  url: projectUrl,
-  title: projectTitle,
-  desc: projectDesc,
-  thumb: projectThumb,
+  id: 1,
+  url: 'http://arcg.is/2n5E9Jf',
+  title: 'Book Assignment 1: Web Story map of RJ, Brasil',
+  desc: 'Web Story map of RJ, Brasil',
+  thumb: true,
+  keyword:['Brasil','tour']
 }
-
-// logs the title from the project data array, because the title is in position 2
-console.log(projectArray[2])
-
-// two ways to log the title from the project data object
-console.log(projectObject.title)
-console.log(projectObject['title'])
+console.log(projectArray)
