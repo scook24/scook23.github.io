0bf55610fd11fe892362a1e5a95b349ce601534a
scook23 <35343383+scook23@users.noreply.github.com>
2018-04-22 21:02:40 -0500

    addition of petroleum basemap

--- final/oil.js
+++ final/oil.js
@@ -1,12 +1,20 @@
-let mypumpmap = L.map('pumpmapid').setView([28,-94], 7)
-let myEarthmap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.').addTo(mypumpmap)
-let myTopomap = L.tileLayer ('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png')
-let myOpenmap = L.tileLayer ('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png')
+let mypumpmap = L.map('pumpmapid').setView([30,-94], 6)
+let CartoDB_DarkMatter = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+});
+let OpenInfraMap_Petroleum = L.tileLayer('https://tiles-{s}.openinframap.org/petroleum/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://www.openinframap.org/about.html">About OpenInfraMap</a>'
+}).addTo(mypumpmap);
+let myTopomap = L.tileLayer ('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.')
+let myOpenmap = L.tileLayer ('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}')
 
 let myBasemaps = {
-  'Earth map': myEarthmap,
-  'Topo map': myTopomap,
-  'Open street map': myOpenmap
+  'Petroleum map': (CartoDB_DarkMatter, OpenInfraMap_Petroleum),
+  'Esri World Imagery': myTopomap,
+  'Esri Ocean': myOpenmap
 }
 
 let myLayerStyle = {
