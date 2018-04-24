aa462a80dc3dfdd3a029bb58e671439fa976100c
scook23 <35343383+scook23@users.noreply.github.com>
2018-04-24 11:43:03 -0500

    final project

--- final/oil.js
+++ final/oil.js
@@ -1,9 +1,9 @@
-let mypumpmap = L.map('pumpmapid').setView([30,-94], 6)
+let mypumpmap = L.map('pumpmapid').setView([30,-94], 7)
 let CartoDB_DarkMatter = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
 	subdomains: 'abcd',
 	maxZoom: 19
-});
+}).addTo(mypumpmap);
 let OpenInfraMap_Petroleum = L.tileLayer('https://tiles-{s}.openinframap.org/petroleum/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://www.openinframap.org/about.html">About OpenInfraMap</a>'
